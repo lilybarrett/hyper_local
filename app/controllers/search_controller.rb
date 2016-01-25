86c3597e57809c589f44ecae6175b4cc5682c537
lilybarrett <lily@barrett.us>
2016-01-25 17:17:12 -0500

    complete search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,18 @@
 class SearchController < ApplicationController
   def index
-    if params[:query]
-      @opportunities = Opportunity.where("city ILIKE ?", "%#{params[:query]}%")
+    @opportunities = Opportunity.all
+    if params[:city_query].present?
+      @opportunities = @opportunities.search_by_city(params[:city_query])
     end
 
-    if @opportunities.empty? || params[:query].empty?
-      flash[:notice] = "Sorry, no results."
+    if params[:cause_query].present?
+      @opportunities = @opportunities.search_by_cause(params[:cause_query])
     end
+
+    if params[:city_query].blank? && params[:cause_query].blank?
+      @opportunities = @opportunities.none
+    end
+
+    flash[:notice] = "No results." unless @opportunities.any?
   end
 end
--- spec/opportunities/user_searches_for_opportunities_spec.rb
+++ spec/opportunities/user_searches_for_opportunities_spec.rb
@@ -15,27 +15,27 @@ feature "User searches for opportunities" do
     click_on "Search"
 
     expect(page).to have_content "#{opportunity.description}"
-    expect(page).to_not have_content "Sorry, no results"
+    expect(page).to_not have_content "Sorry! No matches yet."
   end
 
   scenario "user selects cause from drop-down and gets search results" do
     visit "/"
-    select "Technology and Privacy", from: "Cause"
+    select "Technology and Privacy", from: "query"
     click_on "Search"
 
     expect(page).to have_content "#{opportunity.organization.cause.cause}"
-    expect(page).to_not have_content "Sorry, no results"
+    expect(page).to_not have_content "Sorry! No matches yet."
   end
 
   scenario "user types in location, selects cause from drop-down, and gets
   search results" do
     visit "/"
     fill_in "query", with: opportunity.city
-    select "Technology and Privacy", from: "Cause"
+    select "Technology and Privacy", from: "query"
     click_on "Search"
 
     expect(page).to have_content "#{opportunity.organization.cause.cause}"
-    expect(page).to_not have_content "Sorry, no results"
+    expect(page).to_not have_content "Sorry! No matches yet."
   end
 
   scenario "user searches for item that does not return any results" do
@@ -44,6 +44,6 @@ feature "User searches for opportunities" do
     click_on "Search"
 
     expect(page).to_not have_content "#{opportunity.description}"
-    expect(page).to have_content "Sorry, no results"
+    expect(page).to have_content "Sorry! No matches yet."
   end
 end
